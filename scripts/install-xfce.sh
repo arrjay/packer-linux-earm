e900485a409993c6f88a813fd4d7a459ea07ba82
RJ Bergeron <git@untrusted.host>
2019-11-03 17:34:54 -0800

    add missing xfce install

---
+++ scripts/install-xfce.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get -qq -y install xfce4 xfce4-screenshooter xfce4-terminal solaar blueman
+
+printf 'XFCE_PANEL_MIGRATE_DEFAULT=1\n' >> /etc/environment
