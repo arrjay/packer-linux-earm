66aca98ce258476bcc3894e8ae5579473d9626e1
RJ Bergeron <git@untrusted.host>
2019-12-25 13:23:25 -0800

    remove link config from pimd

--- files/pimd/imd.sh
+++ files/pimd/imd.sh
@@ -33,57 +33,6 @@ xml_hn=$(xmlstarlet sel -t -v 'metadata/domain/name' < "${IMD_PATH}")
 
 router_id=$(xmlstarlet sel -t -v metadata/router/@id "${IMD_PATH}")
 
-bridges="$(xmlstarlet sel -t -v 'metadata/domain/devices/interface/source/@bridge' "${IMD_PATH}")"
-
-for b in ${bridges} ; do
-  selpath="metadata/domain/devices/interface[source/@bridge=\"${b}\"]"
-  int_ipv4='' ; int_mac='' ; int_mtu='' ; ext_ifname='' ; ifmac='' ; ifmtu=''
-  int_ipv4="$(xmlstarlet sel -t -v "${selpath}/ipv4/@address" "${IMD_PATH}")"
-  int_mac="$(xmlstarlet sel -t -v "${selpath}/mac/@address" "${IMD_PATH}")"
-  [[ "${int_ipv4}" ]] && [[ "${int_mac}" ]] && {
-    {
-      printf '%s\n' '[Match]'
-      printf 'MACAddress=%s\n' "${int_mac}"
-
-      printf '%s\n' '[Network]'
-      printf 'Address=%s\n' "${int_ipv4}"
-
-      int_mtu="$(xmlstarlet sel -t -v "${selpath}/mtu/@size" "${IMD_PATH}")"
-      [[ "${int_mtu}" ]] && printf '[Link]\nMTUBytes=%s\n' "${int_mtu}"
-    } > "/etc/systemd/network/${b}.network"
-
-    {
-      printf '%s\n' '[Match]'
-      printf 'MACAddress=%s\n' "${int_mac}"
-
-      printf '%s\n' '[Link]'
-      printf 'Name=%s\n' "${b}"
-    } > "/etc/systemd/network/${b}.link"
-
-    # HACK. rename the interface now if needed.
-    for f in /sys/class/net/*/address ; do
-      read -r ifmac < "${f}"
-      [[ "${ifmac}" == "${int_mac}" ]] && {
-        ext_ifname="${f%/address}"
-        ext_ifname="${ext_ifname##*/}"
-        # if it's already the name, evaporate into noop
-        [[ "${ext_ifname}" == "${b}" ]] && ext_ifname=''
-      }
-    done
-    [[ "${ext_ifname}" ]] && {
-      /usr/bin/ip link set "${ext_ifname}" down
-      /usr/bin/ip link set "${ext_ifname}" name "${b}"
-      /usr/bin/ip link set "${b}" up
-    }
-
-    # ditto MTU!
-    [[ -e "/sys/class/net/${b}/mtu" ]] && read -r ifmtu < "/sys/class/net/${b}/mtu"
-    [[ "${int_mtu}" ]] && {
-      /usr/bin/ip link set "${b}" mtu "${int_mtu}"
-    }
-  }
-done
-
 [[ "${router_id}" ]] && {
   printf 'router id %s;\n' "${router_id}" > /etc/bird/router_id.conf
   chown bird:bird /etc/bird/router_id.conf
