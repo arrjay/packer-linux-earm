c02258cc9f921d0b3d86bcf0ba8dfcc835075101
RJ Bergeron <git@untrusted.host>
2019-12-27 17:55:11 -0800

    resync from ospfworker

--- files/pimd/core.bash
+++ files/pimd/core.bash
@@ -7,8 +7,15 @@ err() {
 }
 
 lock() {
-  local path="${1:/tmp/.scriptlock}" retry="${2:0}"
+  local path="${1:-/tmp/.scriptlock}" retry="${2:-0}"
   lockfile-create -r "${retry}" -p "${path}" && return 0
-  err "ERROR: Can't get lock"
+  err "ERROR: Can't get lock at ${path}"
+  return 1
+}
+
+unlock() {
+  local path="${1:-/tmp/.scriptlock}"
+  lockfile-remove "${path}" && return 0
+  err "ERROR: Not your lock at ${path}"
   return 1
 }
