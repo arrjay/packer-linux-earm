10d99c3a7ed4f218807a31eb2bc2bfc77394fc52
RJ Bergeron <git@untrusted.host>
2020-01-11 11:14:51 -0800

    wire initonce

--- files/initonce.sh
+++ files/initonce.sh
@@ -171,7 +171,7 @@ mkdir -p /run/systemd
 mount /boot
 mount / -o remount,rw
 
-sed -i 's| init=/usr/lib/raspi-config/init_resize\.sh||' /boot/cmdline.txt
+sed -i 's| init=/usr/lib/untrustedhose/scripts/initonce\.sh||' /boot/cmdline.txt
 sed -i 's| sdhci\.debug_quirks2=4||' /boot/cmdline.txt
 
 if ! grep -q splash /boot/cmdline.txt; then
---
+++ scripts/common/firstboot.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p /usr/lib/untrustedhost/scripts
+mv /tmp/initonce.sh /usr/lib/untrustedhost/scripts
+chmod +x /usr/lib/untrustedhost/scripts/initonce.sh
+
+sed -ie 's@ init=[0-9a-zA-Z/_.\-]\+@@' /boot/cmdline.txt
+printf ' init=/usr/lib/untrustedhost/scripts/initonce.sh' >> /boot/cmdline.txt
