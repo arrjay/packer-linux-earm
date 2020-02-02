ec30ce1d177c1b423b87399c97eb78683c03159b
RJ Bergeron <git@untrusted.host>
2020-02-01 23:17:36 -0800

    add netdata to base image

--- scripts/common/netdata.sh
+++ scripts/common/netdata.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# install netdata
+# install netdata - omnibus install
 apt-get -qq -y install autoconf autoconf-archive autogen automake curl gcc git gzip libjudy-dev liblz4-dev libmnl-dev libssl-dev libuv1-dev lm-sensors make netcat nodejs pkg-config python python-mysqldb python-psycopg2 python-pymongo tar uuid-dev zlib1g-dev jq python-ipaddress
 cd /usr/src
 git clone https://github.com/netdata/netdata.git
 cd netdata
 git checkout v1.19.0
 ./netdata-installer.sh --dont-start-it --dont-wait
+# disable version check in web dashboard
+echo 0 > /usr/share/netdata/web/version.txt
