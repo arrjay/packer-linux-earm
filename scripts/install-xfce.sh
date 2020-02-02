d9b0786537f2a4713ad5ede11331ee013a74a4ae
RJ Bergeron <git@untrusted.host>
2020-02-01 16:42:02 -0800

    install scdaemon while installing xfce

--- scripts/install-xfce.sh
+++ scripts/install-xfce.sh
@@ -4,6 +4,6 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get -qq -y install xfce4 xfce4-screenshooter xfce4-terminal solaar blueman
+apt-get -qq -y install xfce4 xfce4-screenshooter xfce4-terminal solaar blueman scdaemon
 
 printf 'XFCE_PANEL_MIGRATE_DEFAULT=1\n' >> /etc/environment
