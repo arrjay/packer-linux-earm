9fafc24efb81ce458e6b3f4e37cf44fd36112952
RJ Bergeron <git@untrusted.host>
2020-02-12 20:21:35 -0800

    firewalld hooks for netdata

--- scripts/common/netdata.sh
+++ scripts/common/netdata.sh
@@ -7,5 +7,14 @@ git clone https://github.com/netdata/netdata.git
 cd netdata
 git checkout v1.19.0
 ./netdata-installer.sh --dont-start-it --dont-wait
+
+# force old lm_sensors plugin on
+echo sensors=force >> /etc/netdata/charts.d.conf
+
 # disable version check in web dashboard
 echo 0 > /usr/share/netdata/web/version.txt
+
+# add firewalld service
+firewall-offline-cmd --new-service=netdata
+firewall-offline-cmd --service=netdata --add-port=19999/tcp
+firewall-offline-cmd --service=netdata --set-short=netdata
