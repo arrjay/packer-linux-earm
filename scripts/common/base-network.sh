a1e461d620e3c791ad2d9a6a40197db1450f704c
RJ Bergeron <git@untrusted.host>
2020-04-25 20:17:28 -0700

    AUGH, wpa_supplicant adds p2p-dev- to ifname, call this one wl

--- scripts/common/base-network.sh
+++ scripts/common/base-network.sh
@@ -30,7 +30,7 @@ _EOF_
 cat <<_EOF_ > /etc/systemd/network/zz_default.network
 # fallback - try dhcp on it!
 [Match]
-Name=onboard onboard_wifi
+Name=onboard wl
 [Network]
 LinkLocalAddressing=yes
 LLMNR=true
@@ -45,8 +45,9 @@ _EOF_
 augtool set /files/etc/sysctl.conf/net.ipv6.conf.default.disable_ipv6 1
 
 # configure the wireless interface
-cp /tmp/common/wpa_supplicant-onboard_wifi.conf /etc/wpa_supplicant
-ln -s /lib/systemd/system/wpa_supplicant@.service /etc/systemd/system/multi-user.target.wants/wpa_supplicant@onboard_wifi.service
+printf 'country=%s\n' 'us' >> /etc/wpa_supplicant/wpa_supplicant.conf
+cp /tmp/common/wpa_supplicant-wl.conf /etc/wpa_supplicant
+ln -s /lib/systemd/system/wpa_supplicant@.service /etc/systemd/system/multi-user.target.wants/wpa_supplicant@wl.service
 ln -s /dev/null /etc/systemd/system/systemd-networkd-wait-online.service
 
 # install firewalld
