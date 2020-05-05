6a6d17be00da8d67e293894fec2e0f45eca397f6
RJ Bergeron <git@untrusted.host>
2020-05-04 21:28:27 -0700

    bump kernel version

--- scripts/common/kernelsrc.sh
+++ scripts/common/kernelsrc.sh
@@ -4,7 +4,7 @@ set -e
 
 # the raspberry pi kernel sources from apt-get are a mess, and using git to retrieve kernel sources are silly.
 # have a... "close enough"
-curl -L -o /root/kernel.txz https://hose.g.bbxn.us/linux/kernel/v4.x/linux-4.19.103.tar.xz
+curl -L -o /root/kernel.txz https://hose.g.bbxn.us/linux/kernel/v4.x/linux-4.19.120.tar.xz
 mkdir /usr/src/linux-upstream
 cd /usr/src/linux-upstream
 tar x --strip-components=1 -f /root/kernel.txz -C .
