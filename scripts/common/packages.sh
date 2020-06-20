e5c09dfe0b519b1d8b2412ed13b820453f03e2ff
RJ Bergeron <git@untrusted.host>
2020-06-19 18:08:15 -0700

    chrony setup

--- scripts/common/packages.sh
+++ scripts/common/packages.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-apt-get -qq -y install pi-bluetooth tmux augeas-tools lockfile-progs xmlstarlet ipcalc
+apt-get -qq -y install pi-bluetooth tmux augeas-tools lockfile-progs xmlstarlet ipcalc chrony
+
+# disable chrony
+systemctl disable chrony
+
+# drop makestep snippet
+mkdir -p /etc/untrustedhost/chrony
+printf 'makestep %s %s' '1' '-1' > /etc/untrustedhost/chrony/makestep.conf
