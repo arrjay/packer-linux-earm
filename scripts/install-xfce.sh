f888ab747bd840f544483261927b6a8c2fcf2e22
arrjay <rbergero@gmail.com>
2020-06-20 15:36:32 -0400

    wire autologin user (gfx) as part of xfce base

--- scripts/install-xfce.sh
+++ scripts/install-xfce.sh
@@ -12,3 +12,20 @@ mkdir -p /etc/systemd/system/lightdm.service.d
 printf '[Unit]\nConditionPathExists=/sys/class/backlight/rpi_backlight\n' > /etc/systemd/system/lightdm.service.d/req-backlight.conf
 ln -s /lib/systemd/system/lightdm.service /etc/systemd/system/multi-user.target.wants/lightdm.service
 ln -s /dev/null /etc/systemd/system/plymouth-quit-wait.service
+
+# wire gfx user for autologin via lightdm
+u=gfx
+
+groupadd $u
+useradd -g $u $u
+rsync -a /etc/skel/ /home/$u/
+
+mkdir -p /etc/lightdm/lightdm.conf.d
+printf '[SeatDefaults]\nautologin-user=%s\n' $u > /etc/lightdm/lightdm.conf.d/autologin.conf
+
+# disable light-locker
+mkdir -p /home/$u/.config/autostart
+printf '[Desktop Entry]\nHidden=true\n' > /home/$u/.config/autostart/light-locker.desktop
+
+# finish with this user.
+chown -R $u:$u /home/$u
