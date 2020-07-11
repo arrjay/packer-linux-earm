001d5b40d3d9562075b6a30ffef58f864ce025c3
arrjay <rbergero@gmail.com>
2020-07-11 01:17:53 -0400

    get netdata building for pi and sheeva, from lite layer

--- scripts/netdata.sh
+++ scripts/netdata.sh
@@ -2,16 +2,35 @@
 
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+export LANG=C
+
+. /etc/environment
+export $(awk -F= '{ print $1 }' < /etc/environment)
+
+apt-get -o APT::Sandbox::User=root update
+
+# armel does *not* have nodejs
+case "${PACKER_BUILD_NAME}" in
+  sheeva) extrapkgs=''       ;;
+  *)      extrapkgs='nodejs' ;;
+esac
+
 # install netdata - omnibus install
-apt-get -qq -y install autoconf autoconf-archive autogen automake curl gcc git gzip libjudy-dev liblz4-dev libmnl-dev libssl-dev libuv1-dev lm-sensors make netcat nodejs pkg-config python python-mysqldb python-psycopg2 python-pymongo tar uuid-dev zlib1g-dev jq python-ipaddress firewalld
+apt-get install autoconf autoconf-archive autogen automake curl cmake gcc git gzip libjudy-dev liblz4-dev libmnl-dev libssl-dev libuv1-dev lm-sensors make netcat pkg-config python python-mysqldb python-psycopg2 python-pymongo tar uuid-dev zlib1g-dev jq python-ipaddress firewalld "${extrapkgs}"
+
 cd /usr/src
 git clone https://github.com/netdata/netdata.git
 cd netdata
-git checkout v1.22.1
+git checkout v1.23.1
 ./netdata-installer.sh --dont-start-it --dont-wait
 
-# force old lm_sensors plugin on
-echo sensors=force >> /etc/netdata/charts.d.conf
+case "${PACKER_BUILD_NAME}" in
+  pi)
+    # force old lm_sensors plugin on
+    echo sensors=force >> /etc/netdata/charts.d.conf
+   ;;
+esac
 
 # disable version check in web dashboard
 echo 0 > /usr/share/netdata/web/version.txt
@@ -20,7 +39,5 @@ echo 0 > /usr/share/netdata/web/version.txt
 firewall-offline-cmd --new-service=netdata
 firewall-offline-cmd --service=netdata --add-port=19999/tcp
 firewall-offline-cmd --service=netdata --set-short=netdata
-
-apt-get -qq clean
 
 df -m
