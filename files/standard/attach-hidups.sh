2f858191128610da8f8920798a4285f66f07b6fe
arrjay <rbergero@gmail.com>
2020-08-16 20:33:27 -0400

    fix the ups driver generator to stabilize, work around tripp-lite stupidity

--- files/standard/attach-hidups.sh
+++ files/standard/attach-hidups.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# we are handed one argument - vendorid/serial
-vendorid="${1%/*}"
-serialno="${1#*/}"
+# we are handed one argument - vendorid-serial
+vendorid="${1%-*}"
+serialno="${1#*-}"
 
 # drag dynamic device setup over to nut, ffs this is annoying
 mkdir -p /run/untrustedhost/nut/ups.conf.d
@@ -12,5 +12,21 @@ cat <<EOF>"/run/untrustedhost/nut/ups.conf.d/${vendorid}-${serialno}.conf"
   driver   = usbhid-ups
   port     = auto
   vendorid = ${vendorid}
-  serial   = ${serialno}
 EOF
+
+case "${vendorid}" in
+  # BUG: tripplite device does not return iserial so we unset it.
+  09ae) unset serialno ;;
+esac
+
+[[ "${serialno}" ]] && printf '  serialno = %s\n' "${serialno}"
+
+# also, tell systemd to leave the driver running.
+mkdir -p "/run/systemd/system/nut-driver.service.d"
+cat <<EOF>"/run/systemd/system/nut-driver.service.d/10-dontstop.conf"
+[Unit]
+StopWhenUnneeded=no
+EOF
+systemctl daemon-reload
+
+exit 0
