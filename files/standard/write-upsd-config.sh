0f4fab35bd0602afcd346fdef2f88eb2a2889970
arrjay <rbergero@gmail.com>
2020-08-19 02:23:04 -0400

    initial alias work (doesn't work)

--- files/standard/write-upsd-config.sh
+++ files/standard/write-upsd-config.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-cat /run/untrustedhost/nut/ups.conf.d/*.conf > /run/untrustedhost/nut/ups.conf
+# unwind any aliases now
+mkdir -p /run/untrustedhost/nut/alias.conf.d
+for f in /run/untrustedhost/nut/discovered-ups.xml.d/*.xml ; do
+  [[ -f "${f}" ]] || continue
+  upsname='' ; upsvend='' ; upsser='' ; aliasct='0'
+  upsname="$(xmlstarlet sel -t -v 'ups/@name' "${f}")"
+  upsvend="$(xmlstarlet sel -t -v 'ups/@vendor' "${f}")"
+  upsser="$(xmlstarlet sel -t -v 'ups/@serial' "${f}")"
+  aliasct="$(xmlstarlet sel -t -v 'count(nut/ups[@vendor="'"${upsvend}"'"][@serial="'"${upsser}"'"]/alias)' /run/untrustedhost/nut/conf.xml)"
+  while [[ "${aliasct}" -gt 0 ]] ; do
+    aliasname="$(xmlstarlet sel -t -v 'nut/ups[@vendor="'"${upsvend}"'"][@serial="'"${upsser}"'"]/alias['"${aliasct}"']' /run/untrustedhost/nut/conf.xml)"
+    printf '[%s]\n  driver = dummy-ups\n  port = %s@localhost\n' "${aliasname}" "${upsname}" > "/run/untrustedhost/nut/alias.conf.d/${aliasname}.conf"
+    systemd-run --on-active=1m systemctl start "mdns-publish-cname@ups-${aliasname}"
+    ((aliasct--))
+  done
+done
+
+# fold real and virtual ups units into the file for driver startup.
+printf 'maxretry = %s\n' 3 > /run/untrustedhost/nut/ups.conf
+cat /run/untrustedhost/nut/ups.conf.d/*.conf /run/untrustedhost/nut/alias.conf.d/*.conf >> /run/untrustedhost/nut/ups.conf
 chgrp nut /run/untrustedhost/nut/ups.conf
 chmod 0660 /run/untrustedhost/nut/ups.conf
 
