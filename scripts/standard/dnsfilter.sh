05e03f9a0a1eee7031b23a6cf7833051cd151aec
arrjay <rbergero@gmail.com>
2020-09-02 01:11:51 -0400

    install blocklists for dns filter

--- scripts/standard/dnsfilter.sh
+++ scripts/standard/dnsfilter.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 apt-get install unbound
 
 systemctl disable unbound
+
+unbound-anchor
+
+cd /usr/src
+git clone https://github.com/arrjay/unbound-hosts-blocklists/
+
+mkdir -p /usr/lib/unbound-hosts-blocklists
+
+printf 'workdir=%s\n' '/usr/lib/unbound-hosts-blocklists' > /etc/update-blocklists.conf
+
+/usr/src/unbound-hosts-blocklists/update-blocklists
+
+ln -s /usr/lib/unbound-hosts-blocklists/local-data.conf /etc/unbound/unbound.conf.d/local-data.conf
