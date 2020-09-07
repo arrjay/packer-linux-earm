fd6fa9dc183419452fa8e375fdbef9d26db2cd6f
arrjay <rbergero@gmail.com>
2020-09-06 23:05:21 -0400

    initial miniupnp support

---
+++ scripts/standard/miniupnpd.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+printf '%s\n' "miniupnpd miniupnpd/ip6script boolean false" \
+              "miniupnpd miniupnpd/listen string INTERNAL" \
+              "miniupnpd miniupnpd/force_igd_desc_v1 boolean false" \
+              "miniupnpd miniupnpd/start_daemon boolean false" \
+              "miniupnpd miniupnpd/iface string EXTERNAL" \
+  | debconf-set-selections
+
+apt-get install miniupnpd
