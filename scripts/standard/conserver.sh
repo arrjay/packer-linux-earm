3cfd37cb67b92ed0b172f2491343c1e7d0180279
RJ Bergeron <rj@bbxn.us>
2021-02-23 20:01:57 -0800

    drop network support for now ;)

--- scripts/standard/conserver.sh
+++ scripts/standard/conserver.sh
@@ -4,10 +4,10 @@ VERSION=8.2.6
 
 curl -L -o /tmp/conserver.tgz "https://github.com/bstansell/conserver/releases/download/v${VERSION}/conserver-${VERSION}.tar.gz"
 
-apt-get install libwrap0-dev libpam0g-dev libipmiconsole-dev
+apt-get install libipmiconsole-dev
 
 cd /usr/src
 tar xf /tmp/conserver.tgz
 cd "conserver-${VERSION}"
-./configure --with-uds --with-freeipmi --with-ipv6 --with-pam --with-libwrap
+./configure --with-uds --with-freeipmi
 make install 
