910d125490e32969fbe7a8fa3aafcf6e6fdbd999
RJ Bergeron <git@untrusted.host>
2021-04-09 14:40:08 +0000

    upgrade tang to proper v8 release

--- scripts/standard/tang.sh
+++ scripts/standard/tang.sh
@@ -7,7 +7,7 @@ apt-get install -qq -y ninja-build meson libjose-dev libhttp-parser-dev jose
 cd /usr/src
 git clone https://github.com/latchset/tang
 cd tang
-git checkout 75106616bb7a4255d2303e4b31205cb2ad1d8bfa
+git checkout v8
 mkdir build
 cd build
 meson .. --prefix=/usr
