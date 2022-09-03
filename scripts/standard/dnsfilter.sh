ffd274eff5c3484377a3c8faf52dcc907acec15f
RJ <git@untrusted.host>
2022-09-03 19:47:00 +0000

    for now, disable the dnsfilter stuff

--- scripts/standard/dnsfilter.sh
+++ scripts/standard/dnsfilter.sh
@@ -11,13 +11,13 @@ systemctl disable unbound
 # this thing exits nonzero if it updates the anchor. I hate it.
 unbound-anchor -vv || true
 
-cd /usr/src
-git clone https://github.com/arrjay/unbound-hosts-blocklists/
+#cd /usr/src
+#git clone https://github.com/arrjay/unbound-hosts-blocklists/
 
-mkdir -p /usr/lib/unbound-hosts-blocklists
+#mkdir -p /usr/lib/unbound-hosts-blocklists
 
-printf 'workdir=%s\n' '/usr/lib/unbound-hosts-blocklists' > /etc/update-blocklists.conf
+#printf 'workdir=%s\n' '/usr/lib/unbound-hosts-blocklists' > /etc/update-blocklists.conf
 
-/usr/src/unbound-hosts-blocklists/update-blocklists
+#/usr/src/unbound-hosts-blocklists/update-blocklists
 
-ln -s /usr/lib/unbound-hosts-blocklists/local-data.conf /etc/unbound/unbound.conf.d/local-data.conf
+#ln -s /usr/lib/unbound-hosts-blocklists/local-data.conf /etc/unbound/unbound.conf.d/local-data.conf
