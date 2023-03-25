422ba0d69a01246a33fa3fd93c9b56d9fdb1429c
RJ <git@untrusted.host>
2023-03-25 01:35:39 +0000

    switch dterm to local source :x

--- scripts/standard/install-dterm.sh
+++ scripts/standard/install-dterm.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+PFSRC=/tmp/packer-files
+
 apt-get install libreadline-dev
 
-curl -L -o /tmp/dterm.tgz http://www.knossos.net.nz/downloads/dterm-0.5.tgz
+#curl -L -o /tmp/dterm.tgz http://www.knossos.net.nz/downloads/dterm-0.5.tgz
 
 mkdir -p /usr/src/dterm
-tar xvf /tmp/dterm.tgz -C /usr/src/dterm --strip-components=1
+tar xvf "${PFSRC}/cache/dterm-0.5.tgz" -C /usr/src/dterm --strip-components=1
 
 cd /usr/src/dterm
 make
